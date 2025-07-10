a23a25d356de1c0fd6067358f8d222b703c07996
Sham Safarany <125689014+Shamsafarany@users.noreply.github.com>
2025-07-10 14:09:11 +0300

    ui and button

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const button = document.querySelector("button");
+const cont = document.querySelector(".cookies-cont");
+const x = document.querySelector("h1");
+
+button.addEventListener("click", () => {
+    cont.style.display = "none";
+})
+
+x.addEventListener("click", () => {
+    cont.style.display = "none";
+})
+
